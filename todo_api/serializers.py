cb395bf3f62e2d3dcd7d28ca5eaf7451bcb9aea9
Naamee <naameeakram@gmail.com>
2024-01-27 05:51:21 +0500

    updated serializer to ModelSerializer

--- todo_api/serializers.py
+++ todo_api/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Project
 
-class ProjectSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=150)
-    
-    def create(self, validated_data):
-        return Project.objects.create(**validated_data)
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name']
