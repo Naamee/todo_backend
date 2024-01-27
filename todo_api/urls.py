7ed6b2aad9979323f39f34e8852adc606acae2ac
Naamee <naameeakram@gmail.com>
2024-01-27 06:38:43 +0500

    created detailed views

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProjectsView, TaskView
+from .views import project_list, task_list, project_detail, task_detail
 
 urlpatterns = [
-    path('projects/', ProjectsView),
-    path('tasks/', TaskView),
+    path('projects/', project_list),
+    path('tasks/', task_list),
+    path('projects/<int:pk>/', project_detail),
+    path('tasks/<int:pk>/', task_detail),
 ]
--- todo_api/views.py
+++ todo_api/views.py
@@ -3,12 +3,14 @@ from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 
 # Create your views here.
-
-def ProjectsView(request):
+@csrf_exempt #remove later for security
+def project_list(request):
     if request.method == 'GET':
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -23,8 +25,32 @@ def ProjectsView(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+@csrf_exempt #remove later for security
+def project_detail(request, pk):
+    try:
+        project = Project.objects.get(pk=pk)
+    except Project.DoesNotExist:
+        return HttpResponse(status=404)
 
-def TaskView(request):
+    if request.method == 'GET':
+        serializer = ProjectSerializer(project)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(project, data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        project.delete()
+        return HttpResponse(status=204)
+
+@csrf_exempt #remove later for security
+def task_list(request):
     if request.method == 'GET':
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
@@ -38,3 +64,27 @@ def TaskView(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt #remove later for security    
+def task_detail(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TaskSerializer(task, data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        task.delete()
+        return HttpResponse(status=204)
