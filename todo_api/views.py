6e02a79d93c38005c0b3af11d47d4bf1ae346632
Naamee <naameeakram@gmail.com>
2024-01-30 10:51:42 +0500

    added filter_task action to taskView

--- todo_api/views.py
+++ todo_api/views.py
@@ -3,6 +3,8 @@ from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response  
 
 
 # Create your views here.
@@ -27,3 +29,18 @@ class TaskViewSet(
 ):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @action(detail=False, methods=['GET'])
+    def filter_tasks(self, request):
+        project_id = request.query_params.get('project_id', None)
+        completed = request.query_params.get('completed', None)
+
+        filters = {}
+        if project_id is not None:
+            filters['project'] = project_id
+        if completed is not None:
+            filters['completed'] = completed.lower() == 'true'
+
+        tasks = Task.objects.filter(**filters)
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
