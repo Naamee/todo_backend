03a8f4e01467fc61d00b11cf206cc2b93d297ef0
Naamee <naameeakram@gmail.com>
2024-01-31 15:20:56 +0500

    updated task serializer datetime format

--- todo_api/serializers.py
+++ todo_api/serializers.py
@@ -7,11 +7,13 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
         
 class TaskSerializer(serializers.ModelSerializer):
+    due_date = serializers.DateField(input_formats=['%d/%m/%Y'], format='%d/%m/%Y')
+
     class Meta:
         model = Task
         fields = ['id', 'project', 'title', 'description', 'due_date', 'priority', 'completed']
     
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['due_date'] = instance.due_date.strftime('%d/%m/%Y')
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     representation['due_date'] = instance.due_date.strftime('%d/%m/%Y')
+    #     return representation
