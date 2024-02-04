aa4e38d7ecc88c4fdb298790b1920413f8c55ff0
Naamee <naameeakram@gmail.com>
2024-02-04 15:27:38 +0500

    minor changes

--- drf/settings.py
+++ drf/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
 	"http://127.0.0.1:8000",
-    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
